bed533b86f23f6f41db63294cc500d16eb88ab55
Luis Rovirosa <luisrovirosa@gmail.com>
2016-05-28 19:45:01 +0200

    refactor extract method "rollScore"

--- src/com/luisrovirosa/katas/bowling/Bowling.java
+++ src/com/luisrovirosa/katas/bowling/Bowling.java
@@ -4,12 +4,16 @@ public class Bowling {
     public int scoreOf(String rolls) {
         int result = 0;
         for (int i = 0; i < 20; i++){
-            String score = rolls.substring(i, i+1);
-            if (score.equals("-")){
-                score = "0";
-            }
-            result += Integer.parseInt(score);
+            result += rollScore(rolls, i);
         }
         return result;
+    }
+
+    private int rollScore(String rolls, int numberOfRoll) {
+        String score = rolls.substring(numberOfRoll, numberOfRoll +1);
+        if (score.equals("-")){
+            score = "0";
+        }
+        return Integer.parseInt(score);
     }
 }
