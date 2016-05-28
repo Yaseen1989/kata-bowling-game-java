16e46dc453ecd9c67fd5e900e4914a3307e8cac1
Luis Rovirosa <luisrovirosa@gmail.com>
2016-05-28 19:48:36 +0200

    Bowling score_10_when_the_first_turn_throw_the_10_pins_in_two_rolls_and_miss_other_rolls

--- src/com/luisrovirosa/katas/bowling/Bowling.java
+++ src/com/luisrovirosa/katas/bowling/Bowling.java
@@ -3,16 +3,18 @@ package com.luisrovirosa.katas.bowling;
 public class Bowling {
     public int scoreOf(String rolls) {
         int result = 0;
-        for (int i = 0; i < 20; i++){
+        for (int i = 0; i < 20; i++) {
             result += rollScore(rolls, i);
         }
         return result;
     }
 
     private int rollScore(String rolls, int numberOfRoll) {
-        String score = rolls.substring(numberOfRoll, numberOfRoll +1);
-        if (score.equals("-")){
+        String score = rolls.substring(numberOfRoll, numberOfRoll + 1);
+        if (score.equals("-")) {
             score = "0";
+        } else if (score.equals("/")) {
+            score = String.valueOf(10 - rollScore(rolls, numberOfRoll-1));
         }
         return Integer.parseInt(score);
     }
--- src/test/com/luisrovirosa/katas/bowling/BowlingShould.java
+++ src/test/com/luisrovirosa/katas/bowling/BowlingShould.java
@@ -14,18 +14,23 @@ public class BowlingShould {
     }
 
     @Test
-    public void score_1_when_the_first_roll_throw_one_pin_and_miss_others_rolls(){
+    public void score_1_when_the_first_roll_throw_one_pin_and_miss_others_rolls() {
         assertThat(scoreOf("1-------------------"), is(1));
     }
 
     @Test
-    public void score_2_when_the_first_roll_throw_two_pin_and_miss_others_rolls(){
+    public void score_2_when_the_first_roll_throw_two_pin_and_miss_others_rolls() {
         assertThat(scoreOf("2-------------------"), is(2));
     }
 
     @Test
-    public void score_20_when_all_rolls_throw_one_pin(){
+    public void score_20_when_all_rolls_throw_one_pin() {
         assertThat(scoreOf("11111111111111111111"), is(20));
+    }
+
+    @Test
+    public void score_10_when_the_first_turn_do_a_spare_and_miss_other_rolls() {
+        assertThat(scoreOf("3/------------------"), is(10));
     }
 
     private int scoreOf(String rolls) {
