9357525ab4de67e22d0bd0ba26394fcd3aa44717
Luis Rovirosa <luisrovirosa@gmail.com>
2016-05-28 19:36:22 +0200

    test refactor extract method "scoreOf"

--- src/test/com/luisrovirosa/katas/bowling/BowlingShould.java
+++ src/test/com/luisrovirosa/katas/bowling/BowlingShould.java
@@ -10,20 +10,17 @@ public class BowlingShould {
 
     @Test
     public void score_0_when_failing_all_the_rolls() {
-        Bowling bowling = new Bowling();
-
-        int score = bowling.scoreOf("--------------------");
-
-        assertThat(score, is(0));
+        assertThat(scoreOf("--------------------"), is(0));
     }
 
     @Test
     public void score_1_when_the_first_roll_throw_one_pin_and_miss_others_rolls(){
+        assertThat(scoreOf("1-------------------"), is(1));
+    }
+
+    private int scoreOf(String rolls) {
         Bowling bowling = new Bowling();
-
-        int score = bowling.scoreOf("1-------------------");
-
-        assertThat(score, is(1));
+        return bowling.scoreOf(rolls);
     }
 
 }
