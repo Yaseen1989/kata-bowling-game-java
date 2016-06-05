28ee456fd9a82d41ab82b425b2e69e4ffe1548a9
Luis Rovirosa <luisrovirosa@gmail.com>
2016-06-05 20:29:30 +0200

    refactor: extract method normalTurns

--- src/com/luisrovirosa/katas/bowling/TurnParser.java
+++ src/com/luisrovirosa/katas/bowling/TurnParser.java
@@ -5,19 +5,24 @@ import java.util.ArrayList;
 public class TurnParser {
 
     public ArrayList<Turn> parse(ArrayList<Roll> scores) {
-        ArrayList<Turn> turns = new ArrayList<>();
-        int numberOfRoll = 0;
-        Turn turn = null;
-        for (int i = 0; i < 10; i++) {
-            turn = getTurn(scores, numberOfRoll);
-            turns.add(turn);
-            numberOfRoll += turn.isStrike() ? 1 : 2;
-        }
-        turns.add(bonus(turn, scores));
+        ArrayList<Turn> turns = normalTurns(scores);
+        turns.add(bonusTurn(scores, turns));
         return turns;
     }
 
-    private Turn bonus(Turn lastTurn, ArrayList<Roll> scores) {
+    private ArrayList<Turn> normalTurns(ArrayList<Roll> scores) {
+        ArrayList<Turn> turns = new ArrayList<>();
+        int numberOfRoll = 0;
+        for (int i = 0; i < 10; i++) {
+            Turn turn = getTurn(scores, numberOfRoll);
+            turns.add(turn);
+            numberOfRoll += turn.isStrike() ? 1 : 2;
+        }
+        return turns;
+    }
+
+    private Turn bonusTurn(ArrayList<Roll> scores, ArrayList<Turn> turns) {
+        Turn lastTurn = turns.get(turns.size() - 1);
         if (lastTurn.isStrike()){
             return new Turn(scores.get(scores.size()-2), scores.get(scores.size()-1));
         }
