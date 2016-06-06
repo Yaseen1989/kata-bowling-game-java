8b07eb14427063c37cb76007dccb6cc6ee7eb240
Luis Rovirosa <luisrovirosa@gmail.com>
2016-06-06 18:39:54 +0200

    refactor: reduce complexity. Remove alien concepts BonusFrame and NoBonusFrame

--- src/com/luisrovirosa/katas/bowling/frame/FrameParser.java
+++ src/com/luisrovirosa/katas/bowling/frame/FrameParser.java
@@ -1,25 +1,15 @@
 package com.luisrovirosa.katas.bowling.frame;
 
-import com.luisrovirosa.katas.bowling.frame.bonus.BonusFrame;
-import com.luisrovirosa.katas.bowling.frame.bonus.NoBonusFrame;
 import com.luisrovirosa.katas.bowling.roll.Roll;
 
 import java.util.ArrayList;
 
 public class FrameParser {
 
-    private static final int NUMBER_OF_TURNS = 10;
-
     public ArrayList<Frame> parse(ArrayList<Roll> rolls) {
-        ArrayList<Frame> frames = normalFrames(rolls);
-        frames.add(bonusFrame(lastTurn(frames), rolls));
-        return frames;
-    }
-
-    private ArrayList<Frame> normalFrames(ArrayList<Roll> rolls) {
         ArrayList<Frame> frames = new ArrayList<>();
         int numberOfRoll = 0;
-        for (int i = 0; i < NUMBER_OF_TURNS; i++) {
+        while (numberOfRoll < rolls.size()){
             Frame frame = getTurn(numberOfRoll, rolls);
             frames.add(frame);
             numberOfRoll += frame.isStrike() ? 1 : 2;
@@ -27,51 +17,19 @@ public class FrameParser {
         return frames;
     }
 
-    private BonusFrame bonusFrame(Frame lastFrame, ArrayList<Roll> rolls) {
-        if (lastFrame.isSpare()) {
-            return spareBonus(rolls);
-        }
-        if (lastFrame.isStrike()) {
-            return strikeBonus(rolls);
-        }
-        return noBonus();
-    }
-
-
     private Frame getTurn(int startingRoll, ArrayList<Roll> rolls) {
-        Roll firstRoll = rolls.get(startingRoll);
+        Roll firstRoll = getRoll(startingRoll, rolls);
         if (firstRoll.numberOfKnockedPins() == 10) {
             return new Strike(firstRoll);
         }
-        Roll secondRoll = rolls.get(startingRoll + 1);
+        Roll secondRoll = getRoll(startingRoll + 1, rolls);
         if (firstRoll.numberOfKnockedPins() + secondRoll.numberOfKnockedPins() == 10) {
             return new Spare(firstRoll, secondRoll);
         }
         return new Frame(firstRoll, secondRoll);
     }
 
-    private Frame lastTurn(ArrayList<Frame> frames) {
-        return frames.get(frames.size() - 1);
-    }
-
-    private NoBonusFrame noBonus() {
-        return new NoBonusFrame();
-    }
-
-    private BonusFrame spareBonus(ArrayList<Roll> rolls) {
-        Roll lastRoll = getRollStartingByTheEnd(0, rolls);
-        return new BonusFrame(lastRoll);
-    }
-
-    private BonusFrame strikeBonus(ArrayList<Roll> rolls) {
-        Roll lastRoll = getRollStartingByTheEnd(0, rolls);
-        Roll penultimateRoll = getRollStartingByTheEnd(1, rolls);
-
-        return new BonusFrame(penultimateRoll, lastRoll);
-    }
-
-    private Roll getRollStartingByTheEnd(int positionFromEnd, ArrayList<Roll> rolls) {
-        int realPosition = rolls.size() - (positionFromEnd + 1);
-        return rolls.get(realPosition);
+    private Roll getRoll(int rollNumber, ArrayList<Roll> rolls) {
+        return rollNumber < rolls.size() ? rolls.get(rollNumber) : Roll.miss();
     }
 }
--- src/com/luisrovirosa/katas/bowling/frame/bonus/NoBonusFrame.java
+++
@@ -1,7 +0,0 @@
-package com.luisrovirosa.katas.bowling.frame.bonus;
-
-public class NoBonusFrame extends BonusFrame {
-    public NoBonusFrame() {
-        super();
-    }
-}
