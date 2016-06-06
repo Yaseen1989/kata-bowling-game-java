88a6a3f7cfc7524e6db0a7e5be92ddc1971ecdc0
Luis Rovirosa <luisrovirosa@gmail.com>
2016-06-06 17:39:06 +0200

    refactor: rename variable scores -> rolls

--- src/com/luisrovirosa/katas/bowling/line/LineParser.java
+++ src/com/luisrovirosa/katas/bowling/line/LineParser.java
@@ -12,40 +12,40 @@ public class LineParser {
 
     private static final int NUMBER_OF_TURNS = 10;
 
-    public Line parse(ArrayList<Roll> scores) {
-        ArrayList<Frame> frames = normalTurns(scores);
-        Frame bonusFrame = bonusTurn(lastTurn(frames), scores);
+    public Line parse(ArrayList<Roll> rolls) {
+        ArrayList<Frame> frames = normalTurns(rolls);
+        Frame bonusFrame = bonusTurn(lastTurn(frames), rolls);
         return new Line(frames, bonusFrame);
     }
 
-    private ArrayList<Frame> normalTurns(ArrayList<Roll> scores) {
+    private ArrayList<Frame> normalTurns(ArrayList<Roll> rolls) {
         ArrayList<Frame> frames = new ArrayList<>();
         int numberOfRoll = 0;
         for (int i = 0; i < NUMBER_OF_TURNS; i++) {
-            Frame frame = getTurn(numberOfRoll, scores);
+            Frame frame = getTurn(numberOfRoll, rolls);
             frames.add(frame);
             numberOfRoll += frame.isStrike() ? 1 : 2;
         }
         return frames;
     }
 
-    private Frame bonusTurn(Frame lastFrame, ArrayList<Roll> scores) {
+    private Frame bonusTurn(Frame lastFrame, ArrayList<Roll> rolls) {
         if (lastFrame.isSpare()) {
-            return spareBonus(scores);
+            return spareBonus(rolls);
         }
         if (lastFrame.isStrike()) {
-            return strikeBonus(scores);
+            return strikeBonus(rolls);
         }
         return noBonus();
     }
 
 
-    private Frame getTurn(int startingRoll, ArrayList<Roll> scores) {
-        Roll firstRoll = scores.get(startingRoll);
+    private Frame getTurn(int startingRoll, ArrayList<Roll> rolls) {
+        Roll firstRoll = rolls.get(startingRoll);
         if (firstRoll.numberOfKnockedPins() == 10) {
             return new Strike(firstRoll);
         }
-        Roll secondRoll = scores.get(startingRoll + 1);
+        Roll secondRoll = rolls.get(startingRoll + 1);
         if (firstRoll.numberOfKnockedPins() + secondRoll.numberOfKnockedPins() == 10) {
             return new Spare(firstRoll, secondRoll);
         }
@@ -60,20 +60,20 @@ public class LineParser {
         return new Frame(new MissRoll());
     }
 
-    private Frame spareBonus(ArrayList<Roll> scores) {
-        Roll lastRoll = getRollStartingByTheEnd(0, scores);
+    private Frame spareBonus(ArrayList<Roll> rolls) {
+        Roll lastRoll = getRollStartingByTheEnd(0, rolls);
         return new Frame(lastRoll);
     }
 
-    private Frame strikeBonus(ArrayList<Roll> scores) {
-        Roll lastRoll = getRollStartingByTheEnd(0, scores);
-        Roll penultimateRoll = getRollStartingByTheEnd(1, scores);
+    private Frame strikeBonus(ArrayList<Roll> rolls) {
+        Roll lastRoll = getRollStartingByTheEnd(0, rolls);
+        Roll penultimateRoll = getRollStartingByTheEnd(1, rolls);
 
         return new Frame(penultimateRoll, lastRoll);
     }
 
-    private Roll getRollStartingByTheEnd(int positionFromEnd, ArrayList<Roll> scores) {
-        int realPosition = scores.size() - (positionFromEnd + 1);
-        return scores.get(realPosition);
+    private Roll getRollStartingByTheEnd(int positionFromEnd, ArrayList<Roll> rolls) {
+        int realPosition = rolls.size() - (positionFromEnd + 1);
+        return rolls.get(realPosition);
     }
 }
